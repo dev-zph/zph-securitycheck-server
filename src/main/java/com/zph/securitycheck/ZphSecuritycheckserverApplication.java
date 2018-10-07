08c3c270302e909db2c29b596606224cf15724e4
zhangpenghui <zhangpenghui>
2018-10-07 16:50:58 +0800

    da

--- src/main/java/com/zph/securitycheck/ZphSecuritycheckserverApplication.java
+++ src/main/java/com/zph/securitycheck/ZphSecuritycheckserverApplication.java
@@ -3,8 +3,11 @@ package com.zph.securitycheck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ZphSecuritycheckserverApplication {
 
 	public static void main(String[] args) {
